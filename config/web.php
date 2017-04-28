da36a6bb090d65158954f69d9979656373b6eb2c
Alexey <a.bachkalo@gmail.com>
2017-04-28 22:53:07 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -12,7 +12,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'X1-HGhm8apkurQcXBSc1nOyI72awf0Yq',
+            'cookieValidationKey' => '',
         ],
         'user' => [
             'identityClass' => 'app\models\User',
